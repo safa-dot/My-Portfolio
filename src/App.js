df9c713db3e5287f1eb29a7f346a55c2f9460901
safa-dot <dehimisafa@gmail.com>
2023-01-15 18:34:45 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <Layout>
       <Routes>
-        <Route exact path='/' element={<Home />} />
+        <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/projects' element={<Projects />} />
         <Route path='/contact' element={<Contact />} />
